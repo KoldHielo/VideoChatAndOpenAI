97ede143c8fcd1c7819abfaa003d35c820d0ed1e
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-10-13 17:20:32 +0100

    Update routing.py append slash

--- main/routing.py
+++ main/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-  re_path('ws/VideoChat', consumers.VideoChatConsumer.as_asgi()),
+  re_path('ws/VideoChat/', consumers.VideoChatConsumer.as_asgi()),
 ]
