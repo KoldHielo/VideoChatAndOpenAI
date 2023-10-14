1a5cf0ae7fb0d8ac605c0d0eb324be3419dd3ef9
KoldHielo <102391895+KoldHielo@users.noreply.github.com>
2023-10-15 00:03:50 +0100

    Update urls.py
    importing debug_toolbar

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+import debug_toolbar
 
 urlpatterns = [
     path('VideoChat/', views.home, name='home'),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
