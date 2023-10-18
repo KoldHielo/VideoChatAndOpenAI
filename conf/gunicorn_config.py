effa581e84b1775f2250e38cfd02ab70209ccb17
Ubuntu <ubuntu@ip-172-31-18-58.eu-north-1.compute.internal>
2023-10-18 19:37:28 +0000

    Editing conf files

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -3,5 +3,5 @@ pythonpath = '/home/ubuntu/webdev/videochatapp'
 bind = '172.31.18.58:8003'
 workers = 1
 worker_class = 'uvicorn.workers.UvicornWorker'
-certfile = '/etc/letsencrypt/live/kieranoldfield.co.uk/fullchain.pem'
-keyfile = '/etc/letsencrypt/live/kieranoldfield.co.uk/privkey.pem'
+certfile = '/etc/letsencrypt/live/kieranoldfield.co.uk-0001/fullchain.pem'
+keyfile = '/etc/letsencrypt/live/kieranoldfield.co.uk-0001/privkey.pem'
