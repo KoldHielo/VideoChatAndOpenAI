0a3a3636412d2e2ae2706969798ec420912c438a
Kieran Oldfield <info@kieranoldfield.co.uk>
2023-10-24 23:43:41 +0000

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def home(request):
+    if request.META['HTTP_HOST'] == '16.170.98.35':
+        settings.DEBUG = True
     return render(request, 'home.html')
